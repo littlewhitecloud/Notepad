839b5dac1d6cf658620182ffb55cca0d401b79f4
小白云 <71159641+littlewhitecloud@users.noreply.github.com>
2023-08-10 12:59:41 +0800

    more stuff!
    menu not able to use  because menubutton looks laggy

--- notepad/main.py
+++ notepad/main.py
@@ -1,4 +1,4 @@
-from ctypes import byref, c_char_p, c_int, sizeof, windll
+from ctypes import byref, c_int, sizeof, windll
 from tkinter import Tk
 from tkinter.messagebox import askyesnocancel
 
@@ -15,31 +15,32 @@ class Window(Tk):
 
         self.title("Notepad")
         self.geometry("1275x665")
-        self.iconbitmap("")
+        self.iconbitmap("notepad.ico")
 
         self.theme = (
-            "dark" if isDark else "light"
+            "dark" if isDark() else "light"
         )  # TODO: move it to the settings later
 
         set_theme(self.theme)
-        self.applyeffect()
+        if self.theme == "dark":
+            self.applyeffect()
+            ApplyMica(
+                windll.user32.GetParent(self.winfo_id()), True
+            )  # TODO: make light theme able to use in light mode
 
         self.editor = Editor()
         self.editor.pack(fill="both", expand=True)
 
         self.protocol("WM_DELETE_WINDOW", self.asksave)
 
-        ApplyMica(windll.user32.FindWindowW(c_char_p(None), "Notepad"), self.theme)
-
     def applyeffect(self) -> None:
         """Apply Mica effect to the window and also dark the titlebar"""
-        value = c_int(2 if self.theme == "dark" else 0)
+        value = c_int(2)
         windll.dwmapi.DwmSetWindowAttribute(
             self.frame(), 20, byref(value), sizeof(value)
         )
 
-        if self.theme == "dark":
-            self["background"] = "black"
+        self["background"] = "black"
 
     def asksave(self) -> None:
         """Ask if you want to save when you want to leave"""
--- notepad/widgets.py
+++ notepad/widgets.py
@@ -1,7 +1,7 @@
-from tkinter import Event, Frame, Misc, Text
+from tkinter import Button, Event, Frame, Misc, Text
 from tkinter.filedialog import askopenfilename
-from tkinter.ttk import Label, Scrollbar
 from tkinter.messagebox import showerror
+from tkinter.ttk import Label, Scrollbar, Separator
 
 from darkdetect import isDark
 
@@ -16,48 +16,90 @@ class AutoHideScrollbar(Scrollbar):
         Scrollbar.set(self, upper, lower)
 
 
+class EditMenubar(Frame):
+    def __init__(
+        self,
+        master: Misc | None = None,
+        background: str = ...,
+    ) -> None:
+        super().__init__(master, background=background)
+
+        self.filebutton = Button(self, text="File")
+        self.editbutton = Button(self, text="Edit")
+        self.viewbutton = Button(self, text="View")
+
+        for target_button in (self.filebutton, self.editbutton, self.viewbutton):
+            target_button.pack(fill="x", side="left", padx=5, pady=2)
+            target_button.config(background=self.master.backgroundcolor, relief="flat")
+
+
 class Editor(Frame):
     def __init__(self, master: Misc | None = None) -> None:
         super().__init__(master=master)
 
+        # Some variable
         self.ln: str = 0
         self.col: str = 1
         self.encoding = "utf-8"  # TODO: move it to the settings later
         self.changed = False
         self.filepath = ""
 
-        self.backgroundcolor = "#020202" if isDark() else "#fffff2"
+        self.backgroundcolor = "#020202" if isDark() else "#F8F8F8"
         self.foregroundcolor = "#CCCCCC" if isDark() else "#595959"
 
-        self.edit = Frame(self)
+        # Create the widgets
+        self.editmenu = EditMenubar(self, background=self.backgroundcolor)
+        self.editmenu.pack(side="top", fill="x")
 
+        self.edit = Frame(self)
         self.text = Text(
             self.edit,
             relief="flat",
             font=("Consolas", 11, "normal"),
             wrap="char",
-            background="#272727",
+            background="#272727" if isDark() else "white",
         )
         self.yscrollbar = AutoHideScrollbar(self.edit)
 
-        self.text.config(yscrollcommand=self.yscrollbar.set)
-        self.yscrollbar.config(command=self.text.yview, orient="vertical")
-
         self.statusbar = Frame(self, height=30, background=self.backgroundcolor)
         self.insertindex = Label(self.statusbar)
+        self.separator = Separator(self.statusbar, orient="vertical")
+        self.separator2 = Separator(self.statusbar, orient="vertical")
+        self.encode = Label(self.statusbar)
+        self.lineend = Label(self.statusbar)
 
+        # Config the widgets
         self.insertindex.config(
             foreground=self.foregroundcolor,
             font=("Segoe UI", 9),
             background=self.backgroundcolor,
         )
+        self.encode.config(
+            foreground=self.foregroundcolor,
+            font=("Segoe UI", 8),
+            background=self.backgroundcolor,
+            text=self.encoding.upper(),
+        )
+        self.lineend.config(
+            foreground=self.foregroundcolor,
+            font=("Segoe UI", 8),
+            background=self.backgroundcolor,
+            text="Windows (CRLF)",
+        )
+        self.text.config(yscrollcommand=self.yscrollbar.set)
+        self.yscrollbar.config(command=self.text.yview, orient="vertical")
 
+        # Pack the widgets
         self.text.pack(fill="both", side="left", expand=True)
         self.yscrollbar.pack(side="right", fill="y")
 
         self.edit.pack(fill="both", expand=True)
 
         self.insertindex.pack(fill="x", side="left", padx=25, pady=2)
+        self.encode.pack(side="right", fill="x", padx=25)
+        self.separator.pack(side="right", fill="y", pady=5)
+        self.lineend.pack(side="right", fill="x", padx=25)
+        self.separator2.pack(side="right", fill="y", pady=5)
 
         self.statusbar.pack(fill="x", side="bottom")
         self.statusbar.pack_propagate(0)
@@ -93,7 +135,7 @@ class Editor(Frame):
         else:
             self.changed = False
 
-    def openfile(self, event: Event) -> None:
+    def openfile(self, event: Event | None = None) -> None:
         """Open file from the filepath"""
         self.filepath = askopenfilename(
             filetypes=(("Text file", "*.txt"), ("Any file", "*.*"))
@@ -102,6 +144,15 @@ class Editor(Frame):
             with open(self.filepath, mode="r", encoding=self.encoding) as f:
                 self.text.insert("insert", f.read())
         except UnicodeDecodeError:
-            showerror("Notepad", "Error, can not decode this file.")
+            showerror("Notepad", "Error, can not decode %s" % self.filepath)
+        except FileNotFoundError:
+            showerror("Notepad", "Error, no such file or directory %s" % self.filepath)
         else:
             self.changed = False
+
+        if self.text.get(0.0, 2.0).find("\n") != -1:
+            self.lineend.config(text="Windows (CRLF)")
+        elif self.text.get(0.0, 2.0).find("\r\n") != -1:
+            self.lineend.config(text="Unix (LF)")
+        else:
+            self.lineend.config(text="??? (CR)")
